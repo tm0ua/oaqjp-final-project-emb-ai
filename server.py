cb175ccb9c72ea8a75673b0817287c6bfb8bf43e
tm0ua <touam@msn.com>
2025-02-12 01:44:33 -0500

    refactored server.py for code analysis

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+Server for the Emotion Detection.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,6 +8,9 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_detector():
+    """
+    Gets the emotion value from the server.
+    """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
@@ -22,14 +28,18 @@ def sent_detector():
     # Check if the dominant_emotion is None, indicating an error or invalid input
     if dominant_emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        # Return a formatted string with the emotion labels and scores
-        return f'''For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, 'joy': {joy}
-         and 'sadness': {sadness}. The dominant emotion is {dominant_emotion}.'''
+
+    # Return a formatted string with the emotion labels and scores
+    return f'''For the given statement, the system response is 'anger': {anger},
+        'disgust': {disgust}, 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}.
+        The dominant emotion is {dominant_emotion}.'''
 
 @app.route("/")
 def render_index_page():
+    """
+    Render the emotion detection UI.
+    """
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8000)
